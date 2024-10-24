bb32a4d127ce2fc690e8c7ad3f1db7cbb035c316
juanmacar15 <jdmadrid19@misena.edu.co>
2024-10-24 18:37:52 -0500

    cambio url imagen
    se sube imagen a un servidor y se cambia la url

--- src/products.js
+++ src/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id:1,
         nombre:'Sal proteinada',
-        img:'../src/images/productos/sal-proteinada.png',
+        img:'https://i.ibb.co/wcR3vrR/sal-proteinada.png',
         precio:'50.000',
         urlPdf:'',
     },
